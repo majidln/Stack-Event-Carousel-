ca18e2897b993ff738b5bb8f54398855caf597a6
Majid Lotfinia <majid.lotfinia@mentorcliq.com>
2021-09-08 18:03:05 +0400

    add animation to carousel

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Image, Dimensions, SafeAreaView, Animated } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, Dimensions, SafeAreaView, Animated, Alert } from 'react-native';
+import { Directions, FlingGestureHandler, State } from 'react-native-gesture-handler'
 
 const { width, height } = Dimensions.get('screen');
 
@@ -49,65 +50,88 @@ export default function App() {
     }).start();
   }, [scrollXIndex]);
 
+  const indexChanged = (newIndex: number) => {
+    setIndex(newIndex);
+    scrollXIndex.setValue(newIndex)
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        contentContainerStyle={{
-          flex: 1,
-          justifyContent: 'center',
-          padding: SPACING * 2,
-          marginTop: 50,
-        }}
-        data={DATA}
-        horizontal
-        inverted={false}
-        scrollEnabled={false}
-        removeClippedSubviews={false}
-        keyExtractor={(item) => item.id}
-        CellRendererComponent={({
-          item,
-          index,
-          children,
-          style,
-          ...props
-        }) => {
-          const newStyle = {...style, zIndex: DATA.length - index };
-          console.log('render cell', index, newStyle)
-          return (
-            <View style={newStyle} index={index} {...props}>
-              {children}
-            </View>
-          );
-        }}
-        renderItem={({ item, index }) => {
-          const inputRange = [index - 1, index, index + 1];
+    <FlingGestureHandler direction={Directions.LEFT}
+      onHandlerStateChange={({ nativeEvent }) => {
+        if (nativeEvent.state === State.END) {
+          if (index < DATA.length - 1) {
+            indexChanged(index + 1)
+          }
+        }
+      }}>
+      <FlingGestureHandler direction={Directions.RIGHT}
+        onHandlerStateChange={({ nativeEvent }) => {
+          if (nativeEvent.state === State.END) {
+            if (index > 0) {
+              indexChanged(index - 1)
+            }
+          }
+        }}>
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            contentContainerStyle={{
+              flex: 1,
+              justifyContent: 'center',
+              padding: SPACING * 2,
+              marginTop: 50,
+            }}
+            data={DATA}
+            horizontal
+            inverted={false}
+            scrollEnabled={false}
+            removeClippedSubviews={false}
+            keyExtractor={(item) => item.id}
+            CellRendererComponent={({
+              item,
+              index,
+              children,
+              style,
+              ...props
+            }) => {
+              const newStyle = { ...style, zIndex: DATA.length - index };
+              console.log('render cell', index, newStyle)
+              return (
+                <View style={newStyle} index={index} {...props}>
+                  {children}
+                </View>
+              );
+            }}
+            renderItem={({ item, index }) => {
+              const inputRange = [index - 1, index, index + 1];
 
-          const translateX = scrollXAnimated.interpolate({
-            inputRange,
-            outputRange: [50, 0, -100],
-          });
+              const translateX = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [50, 0, -100],
+              });
 
-          const scale = scrollXAnimated.interpolate({
-            inputRange,
-            outputRange: [0.8, 1, 1.3],
-          });
+              const scale = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [0.8, 1, 1.3],
+              });
 
-          const opacity = scrollXAnimated.interpolate({
-            inputRange,
-            outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0],
-          });
+              const opacity = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0],
+              });
 
-          return (
-            <Animated.View style={{...styles.itemWrapper, opacity, transform: [{ translateX }, {scale}] }}>
-              <Image source={item.image}
-                resizeMode="cover"
-                style={styles.image}></Image>
-              <Text style={styles.title}>{item.title}</Text>
-            </Animated.View>
-          )
-        }}
-      />
-    </SafeAreaView>
+              return (
+                <Animated.View style={{ ...styles.itemWrapper, opacity, transform: [{ translateX }, { scale }] }}>
+                  <Image source={item.image}
+                    resizeMode="cover"
+                    style={styles.image} />
+                  <Text style={styles.title}>{item.title}</Text>
+                </Animated.View>
+              )
+            }}
+          />
+        </SafeAreaView>
+      </FlingGestureHandler>
+    </FlingGestureHandler>
   );
 }
 
@@ -123,7 +147,8 @@ const styles = StyleSheet.create({
   },
   image: {
     width: IMAGE_WIDTH,
-    height: IMAGE_HEIGHT
+    height: IMAGE_HEIGHT,
+    borderRadius: 12
   },
   title: {
     position: 'absolute',
